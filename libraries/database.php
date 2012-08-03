fbd6f537a3849f9be73cef0251964f20c4c43495
Tim Baker <timbone945@gmail.com>
2012-08-03 11:35:50 -0400

    should have added this sooner :-/ … the new database object class for managing database connections

---
+++ libraries/database.php
@@ -0,0 +1,41 @@
+<?php
+require_once __dir__ . "/settings.php";
+
+class Database {
+
+	private static $dbh;
+
+	private static function startDB() {
+		if( Database::$dbh == null ) {
+	        	Database::$dbh = new PDO('mysql:host=' . $GLOBALS['HOST'] . ';dbname='. $GLOBALS['DATABASE'], $GLOBALS['USERNAME'], $GLOBALS['PASSWORD']);
+	        	Database::$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		}
+	}
+
+	static function query($query_string, $data = array()) {
+		Database::startDB();
+
+		try 
+	       {
+		       	$statement = Database::$dbh->prepare($query_string);
+		       	$statement->execute($data);
+	
+		       	$result = $statement->fetchAll();
+
+	           return $result;
+		}
+		catch(PDOException $ex)
+		{
+			error_log($ex);
+			Database::$dbh = null;
+			return false;
+		}
+
+	}
+
+	function __deconstruct__() {
+		print "destroying DB";
+		Database::$dbh = null;
+	}
+
+}
